4a2a8368785ab5bb38f513bffa91cf1978312873
kentakimura <k.kimura0603@gmail.com>
2025-10-28 23:25:57 +0900

    remove incorrect sigle quatation in ts files

--- step3-fifo-idempotency/bin/fifo-idempotency.ts
+++ step3-fifo-idempotency/bin/fifo-idempotency.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import {'FifoIdemStack'} from '../lib/FifoIdemStack';
+import { FifoIdemStack } from '../lib/FifoIdemStack';
 const app = new cdk.App();
 new FifoIdemStack(app, 'FifoIdemStack', {});
--- step5-design-challenges/bin/design-challenges.ts
+++ step5-design-challenges/bin/design-challenges.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import {'DesignChallengesStack'} from '../lib/DesignChallengesStack';
+import { DesignChallengesStack } from '../lib/DesignChallengesStack';
 const app = new cdk.App();
 new DesignChallengesStack(app, 'DesignChallengesStack', {});
